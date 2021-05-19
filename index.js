6c025a468c318879a89597ae10a55e50b2ce7d0c
unknown <axelgrullon@outlook.com>
2021-05-18 21:44:35 -0400

    Modified custom wordlist

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 console.log("start");
 const initial_time = Date.now();
-const words = require("fs").readFileSync("./resources/wordlist.txt").toString().split("\n");
+const words = require("fs").readFileSync("./resources/wordlist_custom.txt").toString().split("\n");
+
+let max_anagrams = 0;
+let max_ana_words;
+let ana_t_number = 0;
+
+const anagrams = new Map();
 
