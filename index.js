8faa1e1efacc071c39be789d9014d6916ac1f6e6
unknown <axelgrullon@outlook.com>
2021-05-19 11:04:32 -0400

    Seeking and sorting words plus the basic program preparation and logic

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-console.log("start");
-const initial_time = Date.now();
+console.log("Begin analysis...");
+const startingTime = Date.now();
 const words = require("fs").readFileSync("./resources/wordlist_custom.txt").toString().split("\n");
-
-let max_anagrams = 0;
-let max_ana_words;
-let ana_t_number = 0;
-
 const anagrams = new Map();
 
+let max_anagrams = 0;
+let max_anagr_words;
+let anagr_t_number = 0;
+
+for (let i = 0; i < words.length; i++) {
+	const sortedWords = words[i].split('').sort().join("");
+	const anagr = anagrams.get(sortedWords);
+	if (anagr) anagr.push(words[i]);
+	else anagrams.set(sortedWords, [words[i]]);
+}
