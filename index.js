ba905af9a6c5d49e0f1671718419cf10c7b072e7
unknown <axelgrullon@outlook.com>
2021-05-19 11:23:28 -0400

    Code refactoring + words database reader and interpreter

--- index.js
+++ index.js
@@ -3,13 +3,21 @@ const startingTime = Date.now();
 const words = require("fs").readFileSync("./resources/wordlist_custom.txt").toString().split("\n");
 const anagrams = new Map();
 
-let max_anagrams = 0;
-let max_anagr_words;
-let anagr_t_number = 0;
 
+let max_anagwords;
+let max_anagnum = 0;
+let anagr_total = 0;
+
+
+console.log("Sort the words and seek them...");
 for (let i = 0; i < words.length; i++) {
 	const sortedWords = words[i].split('').sort().join("");
 	const anagr = anagrams.get(sortedWords);
 	if (anagr) anagr.push(words[i]);
 	else anagrams.set(sortedWords, [words[i]]);
+}
+
+
+anagrams.forEach(word_index => {
+	if (word_index.length > max_anagnum)
 }
