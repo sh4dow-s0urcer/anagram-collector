a6deb6da28b2e81d2b269022a998f0e94990c864
unknown <axelgrullon@outlook.com>
2021-05-20 16:07:49 -0400

    Logical operation complete + some modifications done to the resources

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-console.log("Begin analysis...");
+console.log("Commencing analysis...");
 const startingTime = Date.now();
-const words = require("fs").readFileSync("./resources/wordlist_custom.txt").toString().split("\n");
+const words = require("fs").readFileSync("./resources/wordlist.txt").toString().split("\n");
 const anagrams = new Map();
 
-
-let max_anagwords;
 let max_anagnum = 0;
+let max_anagwords;
 let anagr_total = 0;
 
 
-console.log("Sort the words and seek them...");
 for (let i = 0; i < words.length; i++) {
 	const sortedWords = words[i].split('').sort().join("");
-	const anagr = anagrams.get(sortedWords);
+	const anagr = anagrams.get(sortedWords)
 	if (anagr) anagr.push(words[i]);
 	else anagrams.set(sortedWords, [words[i]]);
 }
 
-
 anagrams.forEach(word_index => {
-	if (word_index.length > max_anagnum)
-}
+	if (word_index.length > max_anagnum) {
+		max_anagnum = word_index.length;
+		max_anagwords = word_index;
+	}
+	if (word_index.length > 1) anagr_total++;
+});
+
+
+const runtime = Date.now() - startingTime;
+
+console.log("Total words: " + words.length);
+console.log("Total anagrams: " + anagr_total);
+console.log("Maximum anagrams found (" + max_anagwords.length + "): " + max_anagwords);
+console.log("Computation time done in: " + runtime + " ms");
